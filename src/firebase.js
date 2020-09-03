b043236c0857f8722b81d89b85168db5466485b1
Waqas Noor <waqas.noor@epicgames.quixel.com>
2020-09-03 18:37:22 +0500

    Adding Firebase config + refactoring application component eleminate class base structure

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCGTOC4c-T28_X4CFGKIkON451p92Hhln4",
+  authDomain: "think-piece-57f90.firebaseapp.com",
+  databaseURL: "https://think-piece-57f90.firebaseio.com",
+  projectId: "think-piece-57f90",
+  storageBucket: "think-piece-57f90.appspot.com",
+  messagingSenderId: "944140974077",
+  appId: "1:944140974077:web:2fd0d76c15d8190d648b94",
+  measurementId: "G-Z5BCPMHSKB",
+};
+
+firebase.initializeApp(firebaseConfig);
+export const firestore = firebase.firestore();
+export default firebase;
